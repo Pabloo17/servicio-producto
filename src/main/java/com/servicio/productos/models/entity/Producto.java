12a0d52d566bb648d20889dfde717e9a66639857
Pablo <Pablo@DESKTOP-NPPIB0P.home>
2021-04-04 13:50:12 +0200

    created producto project

---
+++ src/main/java/com/servicio/productos/models/entity/Producto.java
@@ -0,0 +1,37 @@
+package com.servicio.productos.models.entity;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "productos")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Producto implements Serializable {
+
+  private static final long serialVersionUID = -5305161939946441342L;
+
+  /** identity = autoincremental */
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String nombre;
+  private Double precio;
+
+  @Column(name = "create_at")
+  @Temporal(TemporalType.TIMESTAMP)
+  private LocalDate createAt;
+}
